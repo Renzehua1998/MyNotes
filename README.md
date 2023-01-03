8b9fca28c1806b3d249ec6cb528507fe65abc626
Zehua Ren Laptop <2579734849@qq.com>
2023-01-03 23:17:01 +0800

    数组1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MyNotes
 
 各种课程和知识的自学笔记
+
+- 计算机书籍：https://awesome-programming-books.github.io/
---
+++ 算法/代码随想录算法PDF/01数组/27.移除元素.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # 双指针法
+        slowIndex = 0
+        for fastIndex in range(len(nums)):
+            if nums[fastIndex] != val:
+                nums[slowIndex] = nums[fastIndex]
+                slowIndex += 1
+        return slowIndex
+# @lc code=end
+
